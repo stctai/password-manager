be3d62fc311d655e9e4fe2b82a55d0f6133937aa
ke <kechen@wustl.edu>
2023-11-18 22:32:35 -0800

    Fix a type error

--- password_generator.py
+++ password_generator.py
@@ -12,7 +12,7 @@ characterList += string.punctuation
 def generate_password(length):
     password = []
  
-    for i in range(length):
+    for i in range(int(length)):
         randomchar = random.choice(characterList)
         password.append(randomchar)
 
