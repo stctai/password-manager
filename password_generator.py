ea72a107ae72d7cf95762847dea5072f8d83b903
stctai <84058750+stctai@users.noreply.github.com>
2023-11-28 00:30:37 -0800

    Enhanced password generation with requirements

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,27 @@
 import string
 import random
 
-characterList = ""
-characterList += string.ascii_letters
-characterList += string.digits
-characterList += string.punctuation
 
-
-def generate_password(length):
+def generate_password(length, has_number, has_special_char):
+    characters = ""
+    letters = string.ascii_letters
+    digits = string.digits
+    punctuation = string.punctuation
     password = []
+    count = 0
+
+    characters += letters
+    if has_number:
+        count += 1
+        password.append(random.choice(digits))
+        characters += digits
+    if has_special_char:
+        count += 1
+        password.append(random.choice(punctuation))
+        characters += punctuation
  
-    for i in range(int(length)):
-        randomchar = random.choice(characterList)
-        password.append(randomchar)
+    for i in range(int(length) - count):
+        password.append(random.choice(characters))
+    random.shuffle(password)
 
     return "".join(password)
