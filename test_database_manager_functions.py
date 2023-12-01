bcea2031121aafbb29c8f8ed44b0dccacd6bdc6c
Kenny48620 <kennyleetrade@gmail.com>
2023-12-01 10:00:53 -0800

    Updated test cases

--- test_database_manager_functions.py
+++ test_database_manager_functions.py
@@ -1,4 +1,5 @@
 # Standard library imports
+import string
 import random
 import sys
 import os
@@ -12,12 +13,14 @@ import database_manager   as db
 import password_crypto    as crypto
 import password_generator as generator
 
-MIN_PASSWORD_LENGTH = 1
+MIN_PASSWORD_LENGTH = 6
 MAX_PASSWORD_LENGTH = 10
 NUM_PASSWORD_TESTS  = 5
+HAS_NUM             = True
+HAS_SPECIAL_CHAR    = True
 TEST_PASSWORD       = "test_password"
 TEST_USER_EMAIL     = "test@email.com"
-TEST_USERNAME       = "test_user"
+TEST_USER_NAME      = "test_user"
 TEST_URL            = "https://test.com"
 TEST_APP_NAME       = "test_app"
 
@@ -29,6 +32,55 @@ def test_get_env_master_password():
     master_password = os.getenv("MASTER_PASSWORD")
     # assert that the result matches the expected password
     assert master_password == "password"
+    
+def test_generate_password_has_no_num_no_special_character():
+    '''
+    Test generate_password with random length, no numbers, and no special characters.
+    '''
+    # generate a random number as the length of password
+    password_length     = random.randint(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
+    password            = generator.generate_password(password_length, not HAS_NUM, not HAS_SPECIAL_CHAR)
+    
+    # assert that the password only contains characters
+    assert password.isalpha(), f"Password '{password}' contains numbers or special characters."
+    
+def test_generate_password_with_num_and_special_character():
+    '''
+    Test generate_password with random length, numbers, and special characters.
+    '''
+    # generate a random number as the length of password
+    password_length     = random.randint(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
+    password            = generator.generate_password(password_length, HAS_NUM, HAS_SPECIAL_CHAR)
+    
+    # assert that the password contains at least one number
+    assert any(char.isdigit() for char in password), f"Password '{password}' does not contain any numbers."
+    
+    # assert that the password contains at least one special character
+    special_characters = string.punctuation
+    assert any(char in special_characters for char in password), f"Password '{password}' does not contain any special characters."
+    
+def test_generate_password_with_num_only():
+    '''
+    Test generate_password  with random length and numbers
+    '''
+    # generate a random number as the length of password
+    password_length     = random.randint(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
+    password            = generator.generate_password(password_length, HAS_NUM, not HAS_SPECIAL_CHAR)
+    
+    # assert that the password contains at least one number
+    assert any(char.isdigit() for char in password), f"Password '{password}' does not contain any numbers."
+    
+def test_generate_password_with_special_character_only():
+    '''
+    Test generate_password with random length and special characters
+    '''
+    # generate a random number as the length of password
+    password_length     = random.randint(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
+    password            = generator.generate_password(password_length, not HAS_NUM, HAS_SPECIAL_CHAR)
+    
+    # assert that the password contains at least one special character
+    special_characters = string.punctuation
+    assert any(char in special_characters for char in password), f"Password '{password}' does not contain any special characters."
 
 @pytest.mark.parametrize("dummy_value", range(5))
 def test_generate_encrypt_decrypt_password(dummy_value):
@@ -38,7 +90,7 @@ def test_generate_encrypt_decrypt_password(dummy_value):
     # generate a random number as the length of password
     password_length     = random.randint(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
     # generate a password based on password_length
-    password            = generator.generate_password(password_length)
+    password            = generator.generate_password(password_length, not HAS_NUM, not HAS_SPECIAL_CHAR)
     # encrypte password twice 
     encrypted_password1 = crypto.encrypt_password(password)
     encrypted_password2 = crypto.encrypt_password(password)
@@ -78,7 +130,7 @@ def test_store_passwords(test_connection):
     Test storing passwords in the database.
     """
     # call the function to store data
-    db.store_passwords(TEST_PASSWORD, TEST_USER_EMAIL, TEST_USERNAME, TEST_URL, TEST_APP_NAME)
+    db.store_passwords(TEST_PASSWORD, TEST_USER_EMAIL, TEST_USER_NAME, TEST_URL, TEST_APP_NAME)
     
     # fetch the stored data from the database
     cursor       = test_connection.cursor()
@@ -94,40 +146,34 @@ def test_store_passwords(test_connection):
     # assert that the stored data matches the input data
     assert decrypted_pw == TEST_PASSWORD
     assert result[2]    == TEST_USER_EMAIL
-    assert result[3]    == TEST_USERNAME
+    assert result[3]    == TEST_USER_NAME
     assert result[4]    == TEST_URL
     assert result[5]    == TEST_APP_NAME
 
-def test_find_password(test_connection, capsys):
+def test_find_password(test_connection):
     '''
-    Test fin_password()    
+    Test find_password()    
     '''
     # call the function to find the stored password
-    db.find_password(TEST_APP_NAME)
-
-    # get the printed output using capsys
-    captured       = capsys.readouterr()
-    printed_output = captured.out.strip()
+    found_password = db.find_password(TEST_APP_NAME, TEST_USER_NAME)
 
     # assert that the printed output contains the expected password
-    assert TEST_PASSWORD in printed_output
+    assert TEST_PASSWORD in found_password
 
 def test_find_user(test_connection, capsys):
     '''
     Test find_user()
     '''
     # call the find_users function with the test email
-    db.find_users(TEST_USER_EMAIL)
+    db.find_users(TEST_APP_NAME)
 
     # get the printed output using capsys
-    captured = capsys.readouterr()
+    captured       = capsys.readouterr()
     printed_output = captured.out.strip()
 
     # assert that the printed output contains the expected information
-    assert TEST_PASSWORD   in printed_output
+    assert TEST_USER_NAME  in printed_output
     assert TEST_USER_EMAIL in printed_output
-    assert TEST_USERNAME   in printed_output
-    assert TEST_URL        in printed_output
     
 def teardown_module():
     '''
